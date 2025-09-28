d5f566c00f7dc8f18fb8c3f9223df82c0479e1a5
smallyunet <dev.wangyu@gmail.com>
2025-09-28 23:04:54 +0800

    24th style
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

--- themes/yinplus/source/js/main.js
+++ themes/yinplus/source/js/main.js
@@ -1,16 +1,3 @@
-function getUrlRelativePath() {
-  var url = document.location.toString();
-  var arrUrl = url.split("//");
-
-  var start = arrUrl[1].indexOf("/");
-  var relUrl = arrUrl[1].substring(start);
-
-  if (relUrl.indexOf("?") != -1) {
-    relUrl = relUrl.split("?")[0];
-  }
-  return relUrl;
-}
-
 $(()=> {
   var blocks = document.querySelectorAll("pre code");
   if (window.hljs) {
